a61f866193ca472acca2bb3638f2062775c15eda
LeoB1um <143550009+LeoB1um@users.noreply.github.com>
2025-02-18 02:10:09 +0000

    A very real script here

---
+++ linux/ubuntu/secure-ssh.sh
@@ -0,0 +1,8 @@
+#secure-ssh.sh
+#author inf
+#creates a new ssh usere using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes rppts ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
+
+#now if I was actually making this for real then there would be a script but I dont have a repo for keys setup
